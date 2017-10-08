08ff43203f832cbfb92dc6a5e0d2bf6fa415b040
sanjnau <sanjnau@gmail.com>
2017-10-08 05:48:00 +0000

    added messages

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+$scope.messages=[
+{
+'sender':'USER',
+'text':'HI',
+'time':'10:01AM'
+},
+{
+'sender':'BOT',
+'text':'What can I do for you?',
+'time':'10:01AM'
+},
+{
+'sender':'USER',
+'text':'News About One Direction',
+'time':'10:02AM'
+},
+{
+'sender':'BOT',
+'text':'They are over!!',
+'time':'10:02AM'
+}
+];
 });
